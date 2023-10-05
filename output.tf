7ab6d151e98b266c49c8d65fbd8307bfaa72874a
francemoko17 <francemoko17@gmail.com>
2023-10-05 00:34:39 -0400

    creation of vpc

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "instance_public_ip" {
+  value = aws_instance.ec2-demo.public_ip
+}
+
+output "vpc_id" {
+  value = aws_vpc.vpc1.id
+}
+
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public_subnet1.id,
+    aws_subnet.public_subnet2.id
+
+  ]
+}
